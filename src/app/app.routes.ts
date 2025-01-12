7967e3dd5205b09df3f0a424aed5f91515466841
Lorenzo <suppa.lorenzo.h00@outlook.it>
2025-01-12 23:05:56 +0100

    Ho capito finalmente le rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContattoInserimentoComponent } from './components/contatto-inserimento/contatto-inserimento.component';
+import { ContattoListaComponent } from './components/contatto-lista/lista.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "aggiungi-contatto", pathMatch: 'full' },
-    { path: "aggiungi-contatto", component: ContattoInserimentoComponent },
+    { path: "", component: HomeComponent},
+    { path: "contatto/inserisci", component: ContattoInserimentoComponent },
+    { path: "contatto/lista", component: ContattoListaComponent},
 ];
